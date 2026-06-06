f9bc0c3e547b4bc8e06fcb9fe4d2c3019c7ef8ab
Andre Schutte <andre.schutte2020@outlook.com>
2026-06-06 16:25:04 +0200

    added login and register pages

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using DemoProjectWebsite.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DemoProjectWebsite.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Replace with real authentication (Identity, DB, etc.)
+                if (model.Username == "admin" && model.Password == "password123")
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid login attempt.");
+            }
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Save user to database
+                return RedirectToAction("Login");
+            }
+            return View(model);
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using DemoProjectWebsite.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace DemoProjectWebsite.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
