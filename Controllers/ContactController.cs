864d9c205f15373f938fc65294dbf3ca1fd29619
Andre Schutte <andre.schutte2020@outlook.com>
2026-06-06 17:33:14 +0200

    added more pages

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using DemoProjectWebsite.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoProjectWebsite.Controllers
+{
+    [Authorize]
+    public class ContactController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Save to database or send email
+                ViewBag.Message = "Your contact details have been submitted successfully.";
+                return View();
+            }
+            return View(model);
+        }
+    }
+}
+
